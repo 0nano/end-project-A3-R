9feb4ec8de4c477034ba20254575ce07dafb8afc
Mart-1 <martin.lobel@isen-ouest.yncrea.fr>
2023-06-13 16:19:23 +0200

    correction doublons new_cities

--- recodermartin.R
+++ recodermartin.R
@@ -1,5 +1,5 @@
 #---------------PACKAGES---------------#
-#install.packages("tidyverse")
+library(dplyr)
 
 #---------------LECTURE---------------#
 nb <- 74000 # Nombre de lignes à lire
@@ -39,11 +39,13 @@ accidents2 <- aggregate(rep(1, nrow(data)) ~ format(data$date, "%Y-%U"), data, s
 #1 - On raoute la colone de région au CSV (on merge les infos)
 # Lecture du premier fichier CSV
 data_cities <- read.csv("infos_cities.csv", sep = ",", header = TRUE)
+data_cities <- data_cities %>% group_by(id_code_insee) %>% filter (! duplicated(id_code_insee)) # retirer les doublons
+
 # Lecture du deuxième fichier CSV
 data_accidents <- read.csv("stat_acc_V3.csv", sep = ";", header = TRUE)
 
 # merge les deux datas selon le code_insee
-data_merged <- merge(data_accidents, data_cities,  by = "id_code_insee")
+data_merged <- merge(data_accidents, data_cities,  by.x = "id_code_insee", by.y = "id_code_insee")
 
 newdata <- data_merged[c("id_code_insee","Num_Acc","num_veh","id_usa","date","ville","latitude.x","longitude.x","descr_cat_veh","descr_agglo","descr_athmo","descr_lum","descr_etat_surf","description_intersection","an_nais","age","place","descr_dispo_secu","descr_grav","descr_motif_traj","descr_type_col","department_name", "department_number", "region_name")]
 
@@ -53,7 +55,6 @@ write.csv(newdata, "new_cities.csv", row.names = FALSE)
 
 
 #2 - On créer le jeu de données
-library(dplyr)
 data_nvjeu <- newdata %>% group_by(descr_grav, region_name) %>% summarise(number = n()) %>% ungroup()
 
 
@@ -62,7 +63,6 @@ data_nvjeu <- newdata %>% group_by(descr_grav, region_name) %>% summarise(number
 
 
 
-
-print("--------------------Tests--------------------")
+#--------------------Tests--------------------
 #print(data$descr_grav)
 #print(data$descr_cat_veh)
