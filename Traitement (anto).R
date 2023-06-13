75cbe05b961ea8b529fd1ed9104b6abcf5a44a6c
aaannntttooo <107413153+aaannntttooo@users.noreply.github.com>
2023-06-13 11:55:15 +0200

    Finission de Visualisation part1 et ajout d'une fonctionnalite sur le traitement des données

--- Traitement (anto).R
+++ Traitement (anto).R
@@ -1,6 +1,7 @@
 #preparation des donnees
 
-data <- read.csv("stat_acc_V3.csv", sep=";")
+#data <- read.csv("stat_acc_V3.csv", sep=";")
+data <- read.csv("~/GitHub/end-project-A3-R/stat_acc_V3.csv", sep=";")
 
 #Changement du nom des colonnes
 colnames(data)[1] = "num_accident" #Num_Acc
@@ -12,12 +13,22 @@ colnames(data)[14] = "descr_intersection" #description_intersection
 
 #Changement du type de variables et changement des valeurs de NULL/NA en valeurs moyenne
 data$an_nais <- as.integer(data$an_nais)
-data$age <- as.integer(data$age)
+data$age <- as.numeric(data$age)
 data$place <- as.integer(data$place)
 
-data$an_nais[is.na(data$an_nais)] <- mean(data$an_nais)
-data$age[is.na(data$age)] <- mean(data$age)
-data$place[is.na(data$place)] <- mean(data$place)
+#Recuperation de la valeur mediane sur chaque colonne 
+mean_an_nais <- mean(data$an_nais, na.rm = TRUE)
+mean_age <- mean(data$age, na.rm = TRUE)
+mean_place <- mean(data$place, na.rm = TRUE)
+
+#affectation des valeurs de NA par la valeur mediane
+data$an_nais[is.na(data$an_nais)] <- mean_an_nais
+data$age[is.na(data$age)] <- mean_age
+data$place[is.na(data$place)] <- mean_place
+
+#Modification de l'age pour obtenir sa valeur au moment de l'accident
+data$age = 2009-data$an_nais 
 
 #conversion du format chr en format date
 data$date <- as.POSIXct(data$date,'%Y-%m-%d %H:%M:%S')
+
--- visualisation_data.R
+++ visualisation_data.R
@@ -1,6 +1,6 @@
 data <- read.csv("stat_acc_V3.csv", sep=";")
-data <- read.csv("~/GitHub/end-project-A3-R/stat_acc_V3.csv", sep=";")
-# Get value counts for the 'pays' column
+#data <- read.csv("~/GitHub/end-project-A3-R/stat_acc_V3.csv", sep=";")
+
 
 #Nombre d’accidents en fonction des conditions atmosphériques
 data_sum <- table(data$descr_athmo)
@@ -15,26 +15,40 @@ barplot(sort(data_sum, decreasing = TRUE), main = "Nombre d’accidents en fonct
 data_sum <- table(data$descr_grav)
 barplot(sort(data_sum, decreasing = TRUE), main = "Nombre d’accidents selon la gravité", xlab = "Gravité", ylab = "Nbr d'accidents",col="#69b3a2")
 
-xnombre_accidents <- table(data$date)
-heures <- format(nombre_accidents, "%H:%M:%S")
-tranches <- cut(data$date, breaks = "hour")
-barplot(tranches, main = "Nombre d'accidents par tranche d'heure", xlab = "Tranche d'heure", ylab = "Nombre d'accidents")
 
-
-plot(x=temps, y=data$date, type="h", ylim=c(-250,50), ylab="")
-
-###Zone de test
-data$date <- strptime(data$date, '%Y-%m-%d %H:%M:%S', tz = '')
-data$date <- format(data$date, '%Y-%m-%d %H:%M:%S')
-
-###
+#Nombre d’accidents par tranches d’heure
+data$date <- as.POSIXct(data$date,'%Y-%m-%d %H:%M:%S')
+hours <- format(data$date, '%H') #Recuperation de l'heure de l'accident 
+breaks <- seq(0, 24, by = 2) # Definition des intervalles de temps, ici par 2h
+time_intervals <- cut(as.numeric(hours), breaks = breaks) #Groupemement des heures avec les intervales correspondantes
+freq_table <- table(time_intervals)
+barplot(freq_table, main = "Nombre d’accidents par tranches d’heure", xlab = "Intervalle de temps", ylab = "Nombre d'accidents",col="#69b3a2")
 
 
 #Nombre d’accidents par ville
 data_sum <- table(data$ville)
-data_ordered <- sort(value_counts, decreasing = TRUE)
-first_cities = head(data_ordered,30)
-#Le premier barplot affiche les 30 premières ville de France avec le plus grand nombre d'accidents, le second, toutes les villes, la lisibilité du second est faible.
+data_ordered <- sort(value_counts, decreasing = TRUE) #Tri par villes avec le plus d'accidents
+first_cities = head(data_ordered,30) #Recuperation des 30 premieres villes
+###Le premier barplot affiche les 30 premières ville de France avec le plus grand nombre d'accidents, le second, toutes les villes, la lisibilité du second est faible.
 barplot(sort(first_cities, decreasing = TRUE), main = "Nombre d’accidents par ville" , ylab = "Nbr d'accidents",col="#69b3a2",las=2)
 #barplot(sort(data_sum, decreasing = TRUE), main = "Nombre d’accidents par ville" , ylab = "Nbr d'accidents",col="#69b3a2",las=2)
+
+
+#Nombre d’accidents par tranches d'age
+#Creation des groupes d'age
+breaks <- seq(-1, 109, by = 10)
+age_intervals <- cut(data$age, breaks = breaks)
+#Affectation des categories avec le nombre d'accidents
+age_sum <- table(age_intervals)
+text(x = barplot(age_sum, main = "Nombre d'accidents par tranches d'âge", 
+                 xlab = "Intervalle d'âge", ylab = "Nombre d'accidents", 
+                 col = "#69b3a2"), y = age_sum, labels = age_sum, pos = 3,col = "#69b3a2",ylim = 25000)
+
+
+
+#Nombre d’accidents par mois
+#data$date <- as.POSIXct(data$date, format = '%Y-%m-%d %H:%M:%S')
+months <- format(data$date, '%m') #Recuperation du mois des accidents
+freq_table <- table(months)
+barplot(freq_table, main = "Nombre d'accidents par mois", xlab = "Mois", ylab = "Nombre d'accidents", col = "#69b3a2", names.arg = month.name)
 
